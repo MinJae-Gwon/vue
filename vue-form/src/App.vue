7940a5a5d3330c22ad1a7d05f437e65722d29600
MinJaeGwon <minjae2271@gmail.com>
2019-12-13 20:52:33 +0900

    input

---
+++ vue-form/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <form v-on:submit='submitForm'>
+    <div>
+      <label for="username">id:</label>
+      <input id="username" type="text" v-model='username'>
+    </div>
+    <div>
+      <label for="password">pw:</label>
+      <input id="password" type="text" v-model='password'>
+    </div>
+    <button type='submit'>LOGIN</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm: function() {
+      console.log(this.username)
+      //console.log(this.username, this.password);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
